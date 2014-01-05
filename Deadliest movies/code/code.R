273379b8a2f332f813b0f6ae7c88ec4bf75d63ab
Simon Garnier <simon.garnier@gmail.com>
2014-01-05 18:41:21 -0500

    Initial commit.
    All files for 1st challenge.

---
+++ Deadliest movies/code/code.R
@@ -0,0 +1,108 @@
+#' Copyright 2014 Simon Garnier (http://www.theswarmlab.com / @sjmgarnier)
+#' 
+#' This script is free software: you can redistribute it and/or modify it under
+#' the terms of the GNU General Public License as published by the Free Software
+#' Foundation, either version 3 of the License, or (at your option) any later
+#' version.
+#' 
+#' This script is distributed in the hope that it will be useful, but WITHOUT
+#' ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#' FOR A PARTICULAR PURPOSE.
+#' 
+#' See the GNU General Public License for more details.
+#' 
+#' You should have received a copy of the GNU General Public License along with
+#' this script. If not, see http://www.gnu.org/licenses/.
+#' 
+
+#' **Document title:** R vs Python - Round 1
+#' 
+#' **Date:** January 5, 2014
+#' 
+#' **Author:** Simon Garnier (http://www.theswarmlab.com / @sjmgarnier)
+#' 
+#' **Description:** This script generates a pretty barchart representing the top 25 most violent movies ordered by number of on screen deaths per minute. For more information, see http://www.theswarmlab.com/r-vs-python-round-1/
+#' 
+#' Document generated with RStudio ([www.rstudio.com](http://www.rstudio.com)).
+#' 
+
+# Load libraries
+library(lattice);       # Very versatile graphics package
+library(latticeExtra);  # Addition to "lattice" that makes layering graphs a breathe, and I'm a lazy person, so why not
+
+# Load data into a data frame
+body.count.data = read.csv('http://files.figshare.com/1332945/film_death_counts.csv');
+
+# Compute on screen deaths per minute for each movie. 
+body.count.data$Deaths_Per_Minute = body.count.data$Body_Count / body.count.data$Length_Minutes;
+
+# Reorder "body.count.data" by (descending) number of on screen deaths per minute
+body.count.data = body.count.data[order(-body.count.data$Deaths_Per_Minute), ];
+
+# Select top 25 most violent movies by number of on screen deaths per minute
+body.count.data = body.count.data[1:25,];
+
+# Combine film title and release date into a new factor column with levels ordered by ascending violence
+Full_Title = paste(body.count.data$Film, ' (', body.count.data$Year, ')', sep='');
+body.count.data$Full_Title = factor(Full_Title, levels = Full_Title[length(Full_Title):1], ordered = TRUE);
+
+# Generate base graph
+graph = barchart(Full_Title ~ Deaths_Per_Minute, data = body.count.data);
+print(graph);
+
+# Create theme
+my.bloody.theme = trellis.par.get();                    # Intialize theme with default value
+my.bloody.theme$axis.line$col = NA;                       # Remove axes 
+my.bloody.theme$plot.polygon$col = '#8A0606';             # Set bar colors to a nice bloody red
+my.bloody.theme$plot.polygon$border = NA;                 # Remove bars' outline
+my.bloody.theme$axis.text$cex = 1;                        # Default axis text size is a bit small. Make it bigger
+my.bloody.theme$layout.heights$bottom.padding = 0;        # Remove bottom padding
+my.bloody.theme$layout.heights$axis.bottom = 0;           # Remove axis padding at the bottom of the graph
+my.bloody.theme$layout.heights$axis.top = 0;              # Remove axis padding at the top of the graph
+
+# Update figure with new theme + other improvements (like a title for instance)
+graph = update(graph, 
+               main='25 most violence packed films by deaths per minute',     # Title of the barchart
+               par.settings = my.bloody.theme,                                # Use custom theme
+               xlab = NULL,                                                   # Remove label of x axis
+               scales=list(x=list(at=NULL)),                                 # Remove rest of x axis
+               xlim = c(0, 6.7),                                              # Set graph limits along x axis to accomodate the additional text (requires some trial and error)
+               box.width=0.75);                                               # Default bar width is a bit small. Make it bigger)
+
+print(graph);
+
+# Combine number of on screen death per minute and duration of the movies into a new character string column
+body.count.data$Deaths_Per_Minute_With_Length = paste(round(body.count.data$Deaths_Per_Minute, digits=2), ' (', body.count.data$Length_Minutes, ' mins)', sep='');
+
+# Add number of on screen deaths per minute and duration of movies at the end of each bar 
+graph = graph + layer(
+  ltext(
+    body.count.data$Deaths_Per_Minute,                # x position of the text
+    25:1,                                             # y position of the text
+    pos=4,                                            # Position of the text relative to the x and y position (4 = to the right)
+    body.count.data$Deaths_Per_Minute_With_Length));  # Text to display                                     
+
+# Print graph
+print(graph);
+
+# Load additional libraries
+library(jpeg);  # To read JPG images
+library(grid);  # Graphics library with better image plotting capabilities
+
+# Download a pretty background image
+download.file(url = 'http://www.theswarmlab.com/wp-content/uploads/2014/01/bloody_gun.jpg', destfile = 'bloody_gun.jpg', quiet = TRUE);
+
+# Load gun image using "readJPEG" from the "jpeg" package
+img = readJPEG('bloody_gun.jpg');
+
+# Add image to graph using "grid.raster" from the "grid" package
+graph = graph + layer_(
+  grid.raster(
+    as.raster(img),                 # Image as a raster
+    x = 1,                          # x location of image "Normalised Parent Coordinates"
+    y = 0,                          # y location of image "Normalised Parent Coordinates"
+    height = 0.7,                   # Height of the image. 1 indicates that the image height is equal to the graph height
+    just = c('right', 'bottom')));  # Justification of the image relative to its x and y locations
+
+# Print graph
+print(graph);
---
+++ Deadliest movies/code/code.py
@@ -0,0 +1,67 @@
+"""
+Copyright 2014 Randal S. Olson
+
+This file is a script that makes pretty bar charts.
+
+This script is free software: you can redistribute it and/or modify it under the
+
+terms of the GNU General Public License as published by the Free Software Foundation,
+
+either version 3 of the License, or (at your option) any later version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this script.
+If not, see http://www.gnu.org/licenses/.
+
+"""
+
+%pylab inline
+from pandas import *
+
+# read the data into a data frame, divide the body counts by the length of the film, and only keep the top 25 highest
+body_count_data = read_csv("http://files.figshare.com/1332945/film_death_counts.csv")
+body_count_data["Deaths_Per_Minute"] = body_count_data["Body_Count"].apply(float).values / body_count_data["Length_Minutes"].values
+body_count_data = body_count_data.sort("Deaths_Per_Minute", ascending=False)[:25]
+body_count_data = body_count_data.sort("Deaths_Per_Minute", ascending=True)
+
+# generate the full titles for the movies: movie name (year)
+full_title = []
+
+for film, year in zip(body_count_data["Film"].values, body_count_data["Year"].values):
+    full_title.append(film + " (" + str(year) + ")")
+    
+body_count_data["Full_Title"] = array(full_title)
+
+# plot the bars
+fig = plt.figure(figsize=(8,12))
+rects = plt.barh(range(len(body_count_data["Deaths_Per_Minute"])), body_count_data["Deaths_Per_Minute"], height=0.8, align="center", color="#8A0707", edgecolor="none")
+
+# plot styling
+yticks(range(len(body_count_data["Full_Title"])), body_count_data["Full_Title"].values, fontsize=14)
+xticks(arange(0, 5, 1), [""])
+ax = axes()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+g = ax.yaxis.tick_left()
+ax.tick_params(axis="y", color="#8A0707")
+ax.tick_params(axis="x", color="white")
+ax.xaxis.grid(color="white", linestyle="-")
+ax.xaxis.tick_bottom()
+
+# this function adds the deaths per minute label to the right of the bars
+def autolabel(rects):
+    for i, rect in enumerate(rects):
+        width = rect.get_width()
+        txt = str(round(float(width), 2)) + " (" + str(body_count_data["Length_Minutes"].values[i]) + " mins)"
+        plt.text(width + 0.25, rect.get_y() + rect.get_height() / 2., txt, ha="left", va="center", fontsize=14)
+
+autolabel(rects)
+
+savefig("25-Violence-Packed-Films.png", bbox_inches="tight")
