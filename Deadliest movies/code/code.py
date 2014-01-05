2bf429a1b26db59e03bc7c0771f35dd37726d8cb
Randy Olson <rso@randalolson.com>
2014-01-05 18:53:32 -0500

    Fix Python code license wording

--- Deadliest movies/code/code.py
+++ Deadliest movies/code/code.py
@@ -1,23 +1,19 @@
 """
 Copyright 2014 Randal S. Olson
 
-This file is a script that makes pretty bar charts.
+This file is a script that makes pretty bar charts. It was written to be executed
+in IPython Notebook.
 
 This script is free software: you can redistribute it and/or modify it under the
-
 terms of the GNU General Public License as published by the Free Software Foundation,
-
 either version 3 of the License, or (at your option) any later version.
 
 This script is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
 See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this script.
 If not, see http://www.gnu.org/licenses/.
-
 """
 
 %pylab inline
