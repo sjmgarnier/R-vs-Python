52da9cac6034a5a1e57d624aa7c16f355d5d0d2f
Randy Olson <rso@randalolson.com>
2014-01-11 21:24:17 -0500

    Changed Python web scraping code
    Only grabs title, year, kills, & IMDB link now

--- Deadliest movies scrape/code/movie-scraper.py
+++ Deadliest movies scrape/code/movie-scraper.py
@@ -23,6 +23,7 @@ If not, see http://www.gnu.org/licenses/.
 """
 
 import string
+import time
 
 # urllib2 reads web pages if you provide it an URL
 import urllib2
@@ -32,7 +33,7 @@ from html2text import html2text
 
 # Generate a list of all letters for the Movie pages (+ a "numbers" page)
 # MovieBodyCount's actor pages are all with capital letters EXCEPT v and x
-letters = ["numbers"] + list(string.letters[26:52].replace("V", "v").replace("X", "x"))
+letters = ["numbers"] + list(string.letters[26:52].upper().replace("V", "v").replace("X", "x"))
 
 list_of_films = []
 
@@ -41,97 +42,69 @@ for letter in letters:
     try:
         # Read the raw HTML from the web page
         page_text = urllib2.urlopen("http://www.moviebodycounts.com/movies-" + letter + ".htm").read()
-		
+        
         # Convert the raw HTML into Markdown
         page_text = html2text(page_text).split("\n")
-		
+        
         # Search through the web page for movie page entries
         for line in page_text:
             # We know it's a movie page entry when it has ".htm" in it, but not ".jpg", "contact.htm", and "movies.htm"
-            # Try looking at the raw Markdown to see why this is
-            if ".htm" in line and ".jpg" not in line and "contact.htm" not in line and "movies.htm" not in line:
-				#print line
-				list_of_films.append(line.split("(")[-1].strip(")"))
-				
+            # .jpg means it's a line with an image -- none of the movie entries have an image
+            # contact.htm and movies.htm means it's a link to the Contact or Movies page -- not what we want
+            # movies- means it's a redirect link to another page -- just skip over it
+            if ".htm" in line and ".jpg" not in line and "contact.htm" not in line and "movies.htm" not in line and "movies-" not in line:
+                #print line
+                # The URL is in between parentheses (), so we can simply split the string on those
+                # Some URLs are full URLs, e.g. www.moviebodycounts.com/movie_name.html, so splitting on the / gives us only the page name
+                list_of_films.append(line.split("(")[-1].strip(")").split("/")[-1])
+                
     # If the movie list page doesn't exist, keep going
     except:
         print "\nerror with " + letter + "\n"
 
-
 # Now that we have every movie web page URL, go through each movie page and extract the movie name, kill counts, etc.
 out_file = open("film-death-counts.csv", "wb")
-for film_page in list_of_films:
+out_file.write("Film,Year,Kill_Count,IMDB_url\n")
 
+for film_page in list_of_films:
     try:
         # The information we're looking for on the page:
         film = ""
-        year = ""
         kills = ""
-        rating = ""
-        directors = ""
-        genre = ""
+        year = ""
+        IMDB_url = ""
         
         # A flag indicating that we've found the film title on the page
         found_title = False
         
         # Read the page's raw HTML and convert it to Markdown (again) and go through each line
         for line in html2text(urllib2.urlopen("http://www.moviebodycounts.com/" + film_page).read()).split("\n"):
-		
+        
             # If we haven't found the title yet, these markers tell us we've found the movie title
             if not found_title and "!" not in line and "(" not in line and "[" not in line and line.strip() != "":
                 film = line
                 found_title = True
-				
+                
             # The kill counts are usually on a line with "Film:"
             if "Film:" in line:
                 kills = line.split()[-1]
 
-            # The genre(s) are usually on a line with "Genre"
-            if "Genre" in line:
-                genre = ""
-				
-                # If there's multiple genres, combine them with bars |
-                try:
-                    for gr in line.split(": ")[1].split(","):
-                        gr = gr.strip()
-                        gr = gr.split("]")[0].strip("[")
-                        genre += gr + "|"
-                    genre = genre.strip("|")
-                except:
-                    genre = ""
-
-            # The director(s) are usually on a line with "Director"
-            if "Director" in line:
-                # Remove non-ASCII characters from the line
-                # Sometimes directors have accents in their name
-                line = line.encode("ascii", "replace")
-				
-                # If there's multiple genres, combine them with bars |
-                try:
-                    directors = line.split(":")[1].strip()
-                    if "[" in directors:
-                        directors = directors.strip("[").split("]")[0]
-                except:
-                    directors = ""
-                
-            # The rating is usually on a line with "Rating"
-            if "Rating" in line:
-                try:
-                    rating = line.split(": ")[1].split()[0]
-                    if rating.lower() == "rated":
-                        rating = line.split(": ")[1].split()[1]
-                except:
-                    rating = ""
-
             # The year is usually on a line with "charts-year"
             if "charts-year" in line:
                 year = line.split("[")[1].split("]")[0]
+            
+            # The IMDB url is on a line with "[imdb]"
+            if "[imdb]" in line.lower():
+                IMDB_url = line.split("(")[-1].split(")")[0]
                 
-        out_file.write(film + "," + year + "," + kills + "," + rating + "," + genre + "," + directors + "\n")
+        out_file.write(film + "," + year + "," + kills + "," + IMDB_url + "\n")
             
     # If a movie page fails to open, print out the error and move on to the next movie
     except Exception as e:
         print film_page
         print e
+		
+    # Be kind and wait a little bit between each request
+	time.sleep(0.1)
         
 out_file.close()
